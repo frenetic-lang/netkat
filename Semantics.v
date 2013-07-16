249a40c834c4bcf83345798dd4aa98017e46284a
canders1 <canders1@swarthmore.edu>
2013-07-16 09:47:00 -0400

    semantics defined

--- Semantics.v
+++ Semantics.v
@@ -16,6 +16,15 @@ Definition get_Packet (hst : history) : packet :=
   end.
 Check empty.
 
+Definition Kleisli {A : Type} (rel1 rel2 : A -> A -> Prop) (ina outa : A) : Prop :=
+  exists (x : A), (rel1 ina x) /\ (rel2 x outa).
+
+Fixpoint iter (rel : history -> history -> Prop) (i : nat) (inh : history) : set history :=
+  match i with
+   | O => singleton inh
+   | S n => (Kleisli rel (iter rel (n))) inh
+  end.
+
 Fixpoint eval (e : exp) (h : history) : set history :=
   match e with
    | Drop => @empty history
@@ -27,5 +36,9 @@ Fixpoint eval (e : exp) (h : history) : set history :=
    | Mod f v => singleton (set_field h f v)
    | Par e1 e2 => (union (eval e1 h) (eval e2 h))
    | Neg e1 => (set_minus (singleton h) (eval e1 h))
-  | _ => @empty history
+   | Obs => singleton (ConsHist (get_Packet h) h)
+   | Seq e1 e2 => Kleisli (eval e1) (eval e2) h
+   | Star e1 => fun (y : history) => exists (i : nat), (iter (eval e1) i h y)
   end.
+
+
