d9a6913e168d4c4c46ef7733c87e86391b75e99c
canders1 <canders1@swarthmore.edu>
2013-07-26 10:18:40 -0400

    cleaned up PHAxioms

--- PHAxioms.v
+++ PHAxioms.v
@@ -18,50 +18,35 @@ Require Import Coq.Lists.List.
 
  Existing Instances Equivalence_exp.
  Local Open Scope equiv_scope.
-
- Lemma PA_Mod_Mod_Comm_Help2:
-   forall (f1 f2 : field) (x : history) (n1 n2 : val), ~(f1 = f2) ->
-     set_field (set_field x f1 n1) f2 n2 = set_field (set_field x f2 n2) f1 n1.
- Proof with auto.
-   intros.
-   destruct x; simpl; rewrite -> Packet.mod_mod_comm...
- Qed.
-      
+  
  Lemma PA_Mod_Mod_Comm:
    forall (f1 f2 : field) (n1 n2 : val), ~(f1 = f2) -> 
     (Seq (Mod f1 n1) (Mod f2 n2)) 
      === (Seq (Mod f2 n2) (Mod f1 n1)).
  Proof with auto.
    intros. split; intros; simpl in *; unfold join in *; destruct H0 as [z [H1 H2]].
-   exists (set_field x f2 n2). split... subst. apply PA_Mod_Mod_Comm_Help2...
-   exists (set_field x f1 n1). split... subst. apply PA_Mod_Mod_Comm_Help2...
- Qed.
-
- Lemma set_other_field_not_effect:
-   forall (f1 f2 : field) (n1 n2 : val) (x : history), ~(f1 = f2) ->
-     (Packet.get_field f2 (get_packet (set_field x f1 n1))) =
-     (Packet.get_field f2 (get_packet x)).
-   Proof with auto.
-     intros. destruct x. destruct p.
-     + destruct f1; destruct f2; simpl; auto; try solve [contradiction H; auto].
-     + destruct f1; destruct f2; simpl; auto; try solve [contradiction H; auto].
-   Qed.
+   exists (set_field x f2 n2). split... subst. destruct x; simpl in *; rewrite Packet.mod_mod_comm;  
+   try solve [reflexivity]; intuition. 
+   exists (set_field x f1 n1). split... subst. destruct x; simpl in *; rewrite Packet.mod_mod_comm;
+   try solve [reflexivity]; intuition. 
+ Qed. 
     
  Lemma PA_Mod_Filter_Comm:
    forall (f1 f2 : field) (n1 n2 : val), ~(f1 = f2) ->
      (Seq (Mod f1 n1) (Match f2 n2)) === (Seq (Match f2 n2) (Mod f1 n1)).
  Proof with auto.
    intros. split ; intros; simpl in *; unfold join in *; destruct H0 as [z [H1 H2]]; subst.
-     remember (Packet.beq_val (Packet.get_field f2 (get_packet (set_field x f1 n1))) n2).
-     destruct b... subst. exists x. split...
-     + rewrite set_other_field_not_effect in Heqb.
-       remember (Packet.beq_val (Packet.get_field f2 (get_packet x)) n2).
-       destruct b. reflexivity. inversion Heqb. trivial. trivial. 
+   remember (Packet.beq_val (Packet.get_field f2 (get_packet (set_field x f1 n1))) n2).
+   destruct b... subst. exists x. split...
+   + rewrite Packet.mod_filter_comm with (f := f1) (n:= n1). destruct x; simpl in *;
+     rewrite <- Heqb; auto.
+     trivial. 
    + contradiction. 
-   + rewrite <- set_other_field_not_effect with (f1 := f1) (n1 := n1) in H1...
+   + exists (set_field x f1 n1). split.  reflexivity.
+     rewrite Packet.mod_filter_comm with (f:= f1) (n := n1) in H1. 
      remember (Packet.beq_val (Packet.get_field f2 (get_packet (set_field x f1 n1))) n2).
-     destruct b. subst. exists (set_field z f1 n1). split... rewrite <- Heqb...
-     contradiction.
+     destruct b; try solve [destruct x;  simpl in *; rewrite <- Heqb in H1; try solve [subst; auto]; auto].
+     trivial.
  Qed.
 
  Lemma obs_doesnt_matter:
@@ -87,14 +72,6 @@ Require Import Coq.Lists.List.
      destruct b. subst... contradiction. trivial.
  Qed.
   
-  Lemma always_true:
-    forall (f1 : field) (n1 : val) (x : history), 
-     Packet.beq_val (Packet.get_field f1 (get_packet (set_field x f1 n1))) n1 = true.
-  Proof with auto.
-    intros; assert (true = Packet.beq_val n1 n1). rewrite Packet.beq_val_true with (v1 := n1) (v2 := n1); reflexivity.
-    destruct x; destruct p; simpl; unfold Packet.set_field; simpl; destruct f1; simpl; rewrite H; reflexivity.
- Qed.
-
   Lemma PA_Mod_Filter:
     forall (f1 : field) (n1 : val),
       (Seq (Mod f1 n1) (Match f1 n1)) === (Mod f1 n1).
@@ -104,14 +81,8 @@ Require Import Coq.Lists.List.
       subst. remember (Packet.beq_val (Packet.get_field f1 (get_packet (set_field x f1 n1))) n1).
       destruct b. subst... contradiction.
     + subst. exists (set_field x f1 n1).
-      split... rewrite always_true...
-  Qed.
-  
-  Lemma Mod_to_Filter: 
-    forall (f1 : field) (n1 : val) (z : history), 
-      (Packet.get_field f1 (get_packet z)) = n1 -> z = set_field z f1 n1.
-  Proof with auto.
-    intros. destruct z; destruct p; simpl; destruct f1; simpl in *; subst...
+      split... destruct x; simpl; rewrite Packet.mod_filter; assert (n1 = n1); try solve [reflexivity];
+      apply Packet.beq_val_true in H; rewrite <- H; reflexivity.
   Qed.
 
   Lemma PA_Filter_Mod:
@@ -121,12 +92,13 @@ Require Import Coq.Lists.List.
     intros. split; intros; simpl in *; unfold join in *.
     + destruct H as [z [H1 H2]].
       subst. remember (Packet.beq_val (Packet.get_field f1 (get_packet x)) n1).
-      destruct b. subst. apply Mod_to_Filter. apply Packet.beq_val_true in Heqb...
-      contradiction.
+      destruct b. subst. destruct z; unfold set_field; simpl in *; 
+      apply Packet.beq_val_true in Heqb; rewrite Packet.filter_mod in Heqb;
+      rewrite Heqb; auto. auto.
     + exists y. split...
       remember (Packet.beq_val (Packet.get_field f1 (get_packet x)) n1).
-      destruct b. subst. apply Packet.beq_val_true in Heqb. apply Mod_to_Filter in Heqb...
-      contradiction.
+      destruct b. subst. apply Packet.beq_val_true in Heqb. destruct y; simpl in *;
+      rewrite Packet.filter_mod in Heqb; rewrite Heqb; reflexivity. contradiction.
   Qed. 
 
   Lemma PA_Mod_Mod:
@@ -153,16 +125,6 @@ Require Import Coq.Lists.List.
       subst; destruct f1; contradiction H; auto]; contradiction.
     + unfold empty in H0. contradiction.
   Qed.
-
- Fixpoint Add_Match_All (all_vals : list val) (f : field) : history -> history -> Prop :=
-    match all_vals with
-      | nil => fun (y x: history) => False
-      | v :: l => union (fun (y x: history) => (match (Packet.beq_val (Packet.get_field f (get_packet x)) v) with
-                                                 | true => True
-                                                 | false => False
-                                               end))
-                        (Add_Match_All l f)
-    end.
 
   Fixpoint Add_Matches (l : list val) (f : field) : exp :=
     match l with 
